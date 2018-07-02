f58d939f962814f1ddd177c160a6d830b43e5e39
Vítor Marçal <git@vitormarcal.com.br>
2018-07-02 10:55:10 -0300

    tentando rodar testes

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,8 +1,8 @@
 class Campaign < ApplicationRecord
   belongs_to :user
   has_many :members, dependent: :destroy
-  before_create :set_member
   before_create :set_status
+  before_create :set_member
   enum status: [:pending, :finished]
   validates :title, :description, :user, :status, presence: true
 
